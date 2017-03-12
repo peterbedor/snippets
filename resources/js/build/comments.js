b72465ac2e19a47c1e7a568f83b7075d001d96c8
Peter <peter@lewiscommunications.com>
2017-03-11 23:19:33 -0600

    Slight comments refactor

--- resources/js/build/comments.js
+++ resources/js/build/comments.js
@@ -1,23 +1,31 @@
 Wee.fn.make('comments', {
 	init: function() {
-		var priv = this.$private;
-
-		priv.bindEvents();
+		this.$private.init();
 
 		Wee.mentions.init();
 	},
 
 	unload: function() {
-		$.events.off('', '.comments')
+		$.events.off('', '.comments');
+		this.$private.path = '';
 	}
 }, {
+	init: function() {
+		this.bindEvents();
+
+		this.url = $.routes.uri().path + '/comments';
+	},
+
 	bindEvents: function() {
 		var scope = this;
 
 		$.events.on({
 			'ref:commentButton': {
 				click: function() {
-					scope.submitComment(false, $('ref:commentBody'));
+					scope.comment(function(data) {
+						$('ref:comments').append(data);
+						$('ref:commentBody').val('');
+					});
 				}
 			},
 			'ref:reply': {
@@ -27,9 +35,35 @@ Wee.fn.make('comments', {
 			},
 			'ref:commentReplyButton': {
 				click: function() {
-					scope.submitComment(
-						$(this).data('id')
-					);
+					var id = $(this).data('id'),
+						$body = $('ref:commentReplyBody-' + id),
+						$replies = $('ref:replies-' + id);
+
+					scope.reply({
+						body: $body.val(),
+						parentId: id
+					}, function(response) {
+						// Append new comment to comments
+						$replies.append(response);
+
+						// Attach tribute event listeners to new textarea
+						Wee.mentions.attach(
+							$replies.children()
+								.last()
+								.find('textarea')[0]
+						);
+
+						// Reset comment form
+						$('ref:commentReply-' + id).toggle();
+						$body.val('');
+
+						// Rebind events
+						$.setRef();
+						scope.rebindEvents();
+
+						// Remove all current mentions from store
+						Wee.mentions.clearMentions();
+					});
 				}
 			}
 		}, {
@@ -37,40 +71,31 @@ Wee.fn.make('comments', {
 		});
 	},
 
-	submitComment: function(id) {
-		var scope = this,
-			$content = $('ref:commentReplyBody-' + id),
-			$context = $content.parents('ref:comment-' + id),
-			slug = $context.data('snippet');
+	comment: function(callback) {
+		this.submitComment({
+			body: $('ref:commentBody').val()
+		}, callback);
+	},
+
+	reply: function(data) {
+		this.submitComment({
+			body: data.body,
+			parentId: data.parentId
+		}, callback);
+	},
+
+	submitComment: function(data, callback) {
+		data = $.extend(data, {
+			mentions: Wee.mentions.getMentions()
+		});
 
 		Wee.api.post({
-			url: '/snippets/' + slug + '/comments',
-			data: {
-				body: $content.val(),
-				parentId: id,
-				mentions: Wee.mentions.getMentions()
-			},
-			success: function(data) {
-				var $reply = $('ref:replies-' + id);
-
-				// Append comment to parent
-				$reply.append(data);
-
-				Wee.mentions.attach(
-					$reply.children().last().find('textarea')[0]
-				);
-
-				// Reset comment form
-				$('ref:commentReply-' + id).toggle();
-				$content.val('');
-
-				$.setRef();
-
-				scope.rebindEvents();
-
-				Wee.mentions.clearMentions();
+			url: this.url,
+			data: data,
+			success: function(response) {
+				callback(response);
 			}
-		});
+		})
 	},
 
 	rebindEvents: function() {
